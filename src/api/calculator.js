731332d6fede9601d6d430822bd60804eca195cf
Aryan-Raj-Cs <aryanrajresonance@gmail.com>
2020-09-11 13:39:06 +0530

    process2

--- src/api/calculator.js
+++ src/api/calculator.js
@@ -14,7 +14,7 @@ const checkType = (req, res, next) => {
 };
 router.get('/',(req,res)=>{
 
-    res.json({message:"hello world"})
+    return  res.json({message:"hello world"})
     
     
     })
@@ -24,7 +24,7 @@ router.post('/add',checkType,(req,res)=>{
 const num1=parseFloat(req.body.num1);
 const num2=parseFloat(req.body.num2);
 if(isNaN(num1)||isNaN(num2)){
-    res.json({
+    return  res.json({
         status:"error",
         message:"Invalid data types",
         
@@ -32,14 +32,14 @@ if(isNaN(num1)||isNaN(num2)){
 }
 
 else if(num1>1000000||num2>1000000){
-    res.json({
+   return res.json({
         status:"error",
-        message:"overflow",
+        message:"Overflow",
        
     })   
 }
 else
-res.json({
+return res.json({
     status:"success",
     message:"the sum of given two numbers",
     sum:num1+num2
@@ -51,7 +51,7 @@ router.post('/sub',checkType,(req,res)=>{
     const num1=parseFloat(req.body.num1);
     const num2=parseFloat(req.body.num2);
     if(isNaN(num1)||isNaN(num2)){
-        res.json({
+        return  res.json({
             status:"error",
             message:"Invalid data types",
             
@@ -59,14 +59,21 @@ router.post('/sub',checkType,(req,res)=>{
     }
     
     else if(num1>1000000||num2>1000000){
-        res.json({
+        return   res.json({
             status:"error",
-            message:"overflow",
+            message:"Overflow",
            
         })   
     }
+
+    else if (num1 - num2 < -1000000) {
+		return res.json({
+			status: `error`,
+			message: `Underflow`,
+        });
+    }
     else
-    res.json({
+    return res.json({
         status:"success",
         message:"the difference of given two numbers",
         difference:num1-num2
@@ -78,7 +85,7 @@ router.post('/sub',checkType,(req,res)=>{
         const num1=parseFloat(req.body.num1);
         const num2=parseFloat(req.body.num2);
         if(isNaN(num1)||isNaN(num2)){
-            res.json({
+            return   res.json({
                 status:"error",
                 message:"Invalid data types",
                 
@@ -86,14 +93,14 @@ router.post('/sub',checkType,(req,res)=>{
         }
         
         else if(num1>1000000||num2>1000000){
-            res.json({
+            return   res.json({
                 status:"error",
-                message:"overflow",
+                message:"Overflow",
                
             })   
         }
         else
-        res.json({
+        return  res.json({
             status:"success",
             message:"The product of given numbers",
             result:num1*num2
@@ -105,7 +112,7 @@ router.post('/sub',checkType,(req,res)=>{
             const num1=parseFloat(req.body.num1);
             const num2=parseFloat(req.body.num2);
             if(isNaN(num1)||isNaN(num2)){
-                res.json({
+                return  res.json({
                     status:"error",
                     message:"Invalid data types",
                     
@@ -113,21 +120,21 @@ router.post('/sub',checkType,(req,res)=>{
             }
             
             else if(num1>1000000||num2>1000000){
-                res.json({
+                return  res.json({
                     status:"error",
-                    message:"overflow",
+                    message:"Overflow",
                    
                 })   
             }
             else if(num2==0){
-                res.json({
+                return  res.json({
                     status:"error",
                     message: "Cannot divide by zero"
                    
                 })  
             }
             else
-            res.json({
+            return  res.json({
                 status:"success",
                 message:"The division of given numbers",
                 result:num1/num2
